259e754b5d2f5ae9c72a3b1f72ff8d056c49ed53
tb255570 <136899792+tb255570@users.noreply.github.com>
2023-07-14 19:58:05 -0500

    Add files via upload

---
+++ youtube_data_extract/ya-webpage/src/components/Thing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CalenderRange from './CalenderRange'
+
+const Thing = () => {
+  return (
+    <div className ='w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        
+        <h1 className ='p-20 font-bold text-center md:text-5xl sm:text-4xl text-2xl md-py-6 '>Select The Date Range</h1>
+                    <div className='mx-auto'>
+                        <CalenderRange />
+                    </div>
+
+                    <div className='p-20'>
+                        <button className='bg-red-800 text-white text-xl w-[200px] rounded-full font-medium my-6 mx-auto py-3'>SUBMIT</button>
+                    </div>
+
+    </div>
+  )
+}
+
+export default Thing
