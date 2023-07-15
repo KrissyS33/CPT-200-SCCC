00a69a71f6329139292f1e35108ad179e14b2010
tb255570 <136899792+tb255570@users.noreply.github.com>
2023-07-14 21:14:37 -0500

    Body.js

--- youtube_data_extract/ya-webpage/src/components/Body.js
+++ youtube_data_extract/ya-webpage/src/components/Body.js
@@ -44,14 +44,6 @@ const Body = () => {
                         <label htmlFor='view-checkbox' className='ml-2 text-xl font-medium'>ANNOTATION CLOSE RATE </label>
                     </div>
 
-                    <h1 className ='p-20 font-bold text-center md:text-5xl sm:text-4xl text-2xl md-py-6 '>Select The Date Range</h1>
-                    <div className='mx-auto'>
-                        <CalenderRange />
-                    </div>
-
-                    <div className='p-6'>
-                        <button className='bg-red-800 text-white text-xl w-[200px] rounded-full font-medium my-6 mx-auto py-3'>SUBMIT</button>
-                    </div>
                     </form>
                 
                 </div>
