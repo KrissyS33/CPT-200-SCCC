4fc4d2a1fec8e124e3b2fc2da994bb59b55dac9a
tb255570 <136899792+tb255570@users.noreply.github.com>
2023-07-14 21:13:38 -0500

    Thing.js

--- youtube_data_extract/ya-webpage/src/components/Thing.js
+++ youtube_data_extract/ya-webpage/src/components/Thing.js
@@ -3,16 +3,18 @@ import CalenderRange from './CalenderRange'
 
 const Thing = () => {
   return (
-    <div className ='w-full h-screen mx-auto text-center flex flex-col justify-center'>
+   <div className ='w-full h-screen mx-auto text-center flex flex-col justify-center'>
         
+      <form action = "/api">
         <h1 className ='p-20 font-bold text-center md:text-5xl sm:text-4xl text-2xl md-py-6 '>Select The Date Range</h1>
-                    <div className='mx-auto'>
-                        <CalenderRange />
-                    </div>
+                        <div className='mx-auto'>
+                            <CalenderRange />
+                        </div>
 
-                    <div className='p-20'>
-                        <button className='bg-red-800 text-white text-xl w-[200px] rounded-full font-medium my-6 mx-auto py-3'>SUBMIT</button>
-                    </div>
+                        <div className='p-20'>
+                            <button className='bg-red-800 text-white text-xl w-[200px] rounded-full font-medium my-6 mx-auto py-3'>SUBMIT</button>
+                        </div>
+      </form>
 
     </div>
   )
