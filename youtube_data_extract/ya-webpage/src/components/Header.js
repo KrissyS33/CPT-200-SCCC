dd17bcc6d40d8088c65e55bd3d793b991c49dbdb
tb255570 <136899792+tb255570@users.noreply.github.com>
2023-07-14 20:17:45 -0500

    Header.js
    updated appearance of header component

--- youtube_data_extract/ya-webpage/src/components/Header.js
+++ youtube_data_extract/ya-webpage/src/components/Header.js
@@ -13,10 +13,10 @@ function Header(){
         <header className="flex justify-between items-center h-24 mx-auto px-4 bg-red-800">
             <h1 className="w-full text-4xl font-bold text-[#ffffff]">YouAnalytics</h1>
             <ul className='hidden md:flex'>
-                <li className='p-4 text-white'>Home</li>
-                <li className='p-4 text-white'>Resources</li>
-                <li className='p-4 text-white'>About</li>
-                <li className='p-4 text-white'>Contact</li>
+                <li className='p-4 text-white font-medium'>Home</li>
+                <li className='p-4 text-white font-medium'>Resources</li>
+                <li className='p-4 text-white font-medium'>About</li>
+                <li className='p-4 text-white font-medium'>Contact</li>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30} />}
@@ -24,10 +24,10 @@ function Header(){
             <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r-red-800 bg-red-800 ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <h1 className="w-full text-4xl font-bold m-4 text-[#ffffff]">YouAnalytics</h1>
                 <ul className='uppercase'>
-                    <li className='p-4 border-b text-white'>Home</li>
-                    <li className='p-4 border-b text-white'>Resources</li>
-                    <li className='p-4 border-b text-white'>About</li>
-                    <li className='p-4 text-white'>Contact</li>
+                    <li className='p-4 border-b text-white font-medium'>Home</li>
+                    <li className='p-4 border-b text-white font-medium'>Resources</li>
+                    <li className='p-4 border-b text-white font-medium'>About</li>
+                    <li className='p-4 text-white font-medium'>Contact</li>
                 </ul>
             </div>
         </header>
