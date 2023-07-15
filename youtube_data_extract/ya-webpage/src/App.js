c00e3723266e475116c5d4e81163dafd9f3cc2ee
tb255570 <136899792+tb255570@users.noreply.github.com>
2023-07-14 20:10:31 -0500

    Update App.js

--- youtube_data_extract/ya-webpage/src/App.js
+++ youtube_data_extract/ya-webpage/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Contact from './components/Contact'
 import About from './components/About'
 import Resources from './components/Resources'
-import THing from './components/Thing'
+import Thing from './components/Thing'
 import Body from './components/Body';
 import Header from './components/Header';
 
